146146cadfcf5c605460234c0508ba1c9bd96834
Paulo-Eduardo <paulo.es@live.com>
2018-06-14 19:25:35 -0300

    Criado endpoint para alterar status da task

--- app/BackEnd/taskAPP/task/urls.py
+++ app/BackEnd/taskAPP/task/urls.py
@@ -4,7 +4,8 @@ from task import views
 
 urlpatterns = [
     url(r'^api/task/$', views.TaskList.as_view()),
-    url(r'^api/task/(?P<pk>[0-9]+)/$', views.TaskList.as_view()),
+    url(r'^api/task/(?P<pk>[0-9]+)/$', views.Task.as_view()),
+    url(r'^api/task/status/(?P<pk>[0-9]+)/$', views.TaskStatus.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- app/BackEnd/taskAPP/task/views.py
+++ app/BackEnd/taskAPP/task/views.py
@@ -4,17 +4,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from task.serializers import TaskSerializer
 from task.models import Task
+from datetime import datetime
 
 # Create your views here.
 class TaskList(APIView):
+    """
+    Endpoints sem pk
+    """
 
+    #Endpoint para criar uma nova tarefa
     def post(self, request, format=None):
         serializer = TaskSerializer(data = request.data)
+        serializer.create = datetime.now
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    #Endpoint para listar todas as tarefas
     def get(self, request, format=None):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
@@ -22,11 +29,40 @@ class TaskList(APIView):
 
 
 class Task(APIView):
-    
+    """
+    Endpoints com pk
+    """
+
+    #Metodo para pegar tarefa especifica
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except CallRecord.DoesNotExist:
+            raise Http404
+
+    #Endpoint para alterar tarefa
     def put(self, request, pk, format=None):
-        taks = self.get_object(pk)
+        task = self.get_object(pk)
+        task.edited = datetime.now
         serializer = TaskSerializer(task)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskStatus(APIView):
+    """
+    Apenas altera status da tarefa com pk enviada na url
+    """
+
+    #Endpoint para alterar status da task
+    def put(self, request, pk, format=None):
+        task = Task.get_object(pk)
+        task.status = not task.status
+        serializer = TaskSerializer(task)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
