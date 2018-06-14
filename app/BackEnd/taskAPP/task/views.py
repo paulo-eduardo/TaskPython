48fffe8f24745eeadf43b5318a4a9eea215acf47
Paulo-Eduardo <paulo.es@live.com>
2018-06-14 18:54:35 -0300

    Criado endpoint para listar tasks e criado url de direcionamento

--- app/BackEnd/taskAPP/task/views.py
+++ app/BackEnd/taskAPP/task/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from task.serializers import TaskSerializer
+from task.models import Task
 
 # Create your views here.
 class TaskList(APIView):
@@ -13,3 +14,8 @@ class TaskList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, format=None):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
